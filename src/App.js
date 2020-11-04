fc44cbc1a88ea6c5cee2127daad828ea130d62d2
tony trinh <tony.w.trinh3@gmail.com>
2020-11-03 21:21:20 -0800

    added jsconfig.json, added monday.listen

--- src/App.js
+++ src/App.js
@@ -13,21 +13,40 @@ class App extends React.Component {
     this.state = {
       settings: {},
       name: "",
+      boardData: null,
+      context: null
     };
   }
 
   componentDidMount() {
     // TODO: set up event listeners
+
+    monday.listen("context", res => {
+      this.setState({context: res.data});
+      console.log(res.data);
+      monday.api(`query ($boardIds: [Int]) { boards (ids:$boardIds) { name items(limit:1) { name column_values { title text } } } }`,
+        { variables: {boardIds: this.state.context.boardIds} }
+      )
+      .then(res => {
+        this.setState({boardData: res.data});
+      });
+    })
   }
 
+ 
+ 
+
   render() {
+    console.log(this.state.settings);
     return (
-      <div class="dashboard-projects border-class-1">
-        <div class="dashboard font-class-2 border-class-1">Dashboard</div>
-        <div class="rectangle-176"></div>
-        <div class="new-project valign-text-middle font-class-1 border-class-1">New Project</div>
+      <div className="dashboard-projects border-class-1">
+        <div className="dashboard font-class-2 border-class-1" >Dashboard</div>
+        <div className="rectangle-176"></div>
+        <div className="new-project valign-text-middle font-class-1 border-class-1">New Project</div>
+
         <Navigation />       
         <List />       
+          {JSON.stringify(this.state.boardData, null, 2)} 
       </div>
     );
   }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,44 +1,43 @@
 import React from "react";
 import "../App";
 
-class Navigation extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // Default state
-    this.state = {
-      settings: {},
-      name: "",
-    };
-  }
-
-  componentDidMount() {
-    // TODO: set up event listeners
-  }
-
-  render() {
-    return (
-      <div id="navBar">
-        <div class="projects valign-text-middle font-class-1 border-class-1">Projects</div>
-        <div class="messages valign-text-middle font-class-1 border-class-1">Messages</div>
-        <div class="proposals valign-text-middle font-class-1 border-class-1">Proposals</div>
-        <div class="documents valign-text-middle font-class-1 border-class-1">Documents</div>
-        <div class="reports valign-text-middle font-class-1 border-class-1">Reports</div>
-        <div class="directory valign-text-middle font-class-1 border-class-1">Directory</div>
-        <div class="calendar valign-text-middle font-class-1 border-class-1">Calendar</div>
-        <div class="admin valign-text-middle font-class-1 border-class-1">Admin</div>  
-        <img
-          class="line-1"
-          src="https://anima-uploads.s3.amazonaws.com/projects/5fa1a82d0aa76a11ee1c02fa/releases/5fa1b2a05d6f58133fc1a5b9/img/line-1@1x.png"
-        />
-        <img
-          class="line-2"
-          src="https://anima-uploads.s3.amazonaws.com/projects/5fa1a82d0aa76a11ee1c02fa/releases/5fa1b2a05d6f58133fc1a5b9/img/line-2@2x.png"
-        />
-        
+const Navigation = (props) => {
+  return (
+    <div id="navBar">
+      <div className="projects valign-text-middle font-class-1 border-class-1">
+        Projects
       </div>
-    )}
-  }
-  
-  export default Navigation;
-  
+      <div className="messages valign-text-middle font-class-1 border-class-1">
+        Messages
+      </div>
+      <div className="proposals valign-text-middle font-class-1 border-class-1">
+        Proposals
+      </div>
+      <div className="documents valign-text-middle font-class-1 border-class-1">
+        Documents
+      </div>
+      <div className="reports valign-text-middle font-class-1 border-class-1">
+        Reports
+      </div>
+      <div className="directory valign-text-middle font-class-1 border-class-1">
+        Directory
+      </div>
+      <div className="calendar valign-text-middle font-class-1 border-class-1">
+        Calendar
+      </div>
+      <div className="admin valign-text-middle font-class-1 border-class-1">
+        Admin
+      </div>
+      <img
+        className="line-1"
+        src="https://anima-uploads.s3.amazonaws.com/projects/5fa1a82d0aa76a11ee1c02fa/releases/5fa1b2a05d6f58133fc1a5b9/img/line-1@1x.png"
+      />
+      <img
+        className="line-2"
+        src="https://anima-uploads.s3.amazonaws.com/projects/5fa1a82d0aa76a11ee1c02fa/releases/5fa1b2a05d6f58133fc1a5b9/img/line-2@2x.png"
+      />
+    </div>
+  );
+};
+
+export default Navigation;
