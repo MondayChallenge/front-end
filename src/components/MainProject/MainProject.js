76265df64d9b2a885f26d7cbadbccb3d3737733b
tony trinh <tony.w.trinh3@gmail.com>
2020-11-21 17:55:57 -0800

    add pics and working on format for main projet page

--- src/components/MainProject/MainProject.js
+++ src/components/MainProject/MainProject.js
@@ -1,14 +1,43 @@
 import React from "react";
 import Navigation from "components/Navigation/Navigation";
+import building1 from "assets/img/building-1.png";
+import interior1 from "assets/img/interior-1.png";
+import interior2 from "assets/img/interior-2.png";
+import interior3 from "assets/img/interior-3.png";
 
 const MainProject = () => {
   return (
     <div className="dashboard-projects">
       <Navigation />
-        <div className="awef">
-
-            
+      <div className="main-project">
+        <h1>Mattamy Development</h1>
+        <div className="main-project__left">
+          <div className="main-project__left__image">
+            <img
+              src={building1}
+              alt="building-1"
+              className="main-project__left__image--1"
+            />
+            <img
+              src={interior1}
+              alt="interior-1"
+              className="main-project__left__image--2"
+            />
+            <img
+              src={interior2}
+              alt="interior-2"
+              className="main-project__left__image--3"
+            />
+            <img
+              src={interior3}
+              alt="interior-3"
+              className="main-project__left__image--4"
+            />
+          </div>
         </div>
+
+        <div className="main-project__right"></div>
+      </div>
     </div>
   );
 };
