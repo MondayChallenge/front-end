2aa9226194404283bddbf7422d96eb1f334892a3
varunsub <varun.subramanyan@gmail.com>
2020-11-23 18:05:14 -0800

    Added headers to apollo client

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -1,17 +1,29 @@
 // import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { ApolloClient } from 'apollo-client'
-import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:1337/graphql'
-})
+  uri: 'http://localhost:1337/graphql',
+});
 
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = sessionStorage.getItem('jwtToken');
+  console.log(`Bearer ${token}`);
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 export const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache()
-})
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 // export default client;
