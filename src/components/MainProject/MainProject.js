aa6d2e92c4c8916a259b1c8417b6263a40b0c734
varunsub <varun.subramanyan@gmail.com>
2020-11-27 13:05:48 -0800

    Made mainproject dynamic and changed route

--- src/components/MainProject/MainProject.js
+++ src/components/MainProject/MainProject.js
@@ -1,35 +1,40 @@
-import React from "react";
-import Navigation from "components/Navigation/Navigation";
-import building1 from "assets/img/building-1.png";
-import interior1 from "assets/img/interior-1.png";
-import interior2 from "assets/img/interior-2.png";
-import interior3 from "assets/img/interior-3.png";
-import headshot from "assets/img/professional_woman_headshot.jpg";
-
-import { getRandomInt } from "components/utils/getRandomInt";
-
-const MainProject = () => {
-  
-
-  React.useEffect(() => {
-  
-  }, []);
-
+import React from 'react';
+import Navigation from 'components/Navigation/Navigation';
+import building1 from 'assets/img/building-1.png';
+import interior1 from 'assets/img/interior-1.png';
+import interior2 from 'assets/img/interior-2.png';
+import interior3 from 'assets/img/interior-3.png';
+import headshot from 'assets/img/professional_woman_headshot.jpg';
+import { useQuery } from '@apollo/client';
+import { getRandomInt } from 'components/utils/getRandomInt';
+import { useHistory } from 'react-router-dom';
+import { getProjects } from '../../apollo/project';
+import { getCurrUser } from '../../apollo/user';
+import { getUserInfo } from '../../apollo/user';
+import { Link } from 'react-router-dom';
+const MainProject = (props) => {
+  const { loading, error, data } = useQuery(getProjects, {
+    variables: { id: props.match.params.id },
+  });
+  const history = useHistory();
+  const { loading: userLoading, error: userError, data: userData } = useQuery(
+    getCurrUser
+  );
   const bids = [
-    { name: "Cupertino Electric, Inc.", type: "Utilities", status: "Awarded" },
-    { name: "Bay Electric", type: "Utilities", status: "Declined" },
-    { name: "A&A Concrete Supply", type: "Concrete", status: "Awarded" },
+    { name: 'Cupertino Electric, Inc.', type: 'Utilities', status: 'Awarded' },
+    { name: 'Bay Electric', type: 'Utilities', status: 'Declined' },
+    { name: 'A&A Concrete Supply', type: 'Concrete', status: 'Awarded' },
     {
-      name: "SAS Stressteel, Inc.",
-      type: "Structural Steel",
-      status: "Awaiting Response",
+      name: 'SAS Stressteel, Inc.',
+      type: 'Structural Steel',
+      status: 'Awaiting Response',
     },
   ];
 
   const bidColors = {
-    Awarded: "success",
-    Awaiting: "warning",
-    Declined: "danger",
+    Awarded: 'success',
+    Awaiting: 'warning',
+    Declined: 'danger',
   };
 
   const renderBidding = (bids, bidColors) => {
@@ -41,9 +46,8 @@ const MainProject = () => {
           <p className="main-project__right__bidding__group--name">{name}</p>
           <p
             className={`main-project__right__bidding__group--status font-color--${
-              bidColors[status.split(" ")[0]]
-            }`}
-          >
+              bidColors[status.split(' ')[0]]
+            }`}>
             {status}
           </p>
           <p className="main-project__right__bidding__group--type">{type}</p>
@@ -54,53 +58,51 @@ const MainProject = () => {
 
   const team = [
     {
-      name: "David Felber",
-      title: "Project Manager",
-      img: "",
+      name: 'David Felber',
+      title: 'Project Manager',
+      img: '',
     },
     {
-      name: "Welsey Thomas",
-      title: "VP of Business Development",
-      img: "",
+      name: 'Welsey Thomas',
+      title: 'VP of Business Development',
+      img: '',
     },
     {
-      name: "Lauren Stevens",
-      title: "Sr. Architect/Designer",
+      name: 'Lauren Stevens',
+      title: 'Sr. Architect/Designer',
       img: headshot,
     },
     {
-      name: "Brad Nichols",
-      title: "Project Engineer",
-      img: "",
+      name: 'Brad Nichols',
+      title: 'Project Engineer',
+      img: '',
     },
   ];
 
-  const backgroundColor = ["orange", "green", "blue", "pink", "purple"];
+  const backgroundColor = ['orange', 'green', 'blue', 'pink', 'purple'];
 
   const renderTeam = (team, backgroundColor) => {
     return team.map((member, i) => {
       const { name, title, img } = member;
 
       const renderImg = (img, name) => {
-        
         const colorPicker = (colors) => {
           const randInt = getRandomInt(0, colors.length);
           return colors[randInt];
         };
 
-        if (img === "" || img === null) {
-          const nameSplit = name.split(" ");
+        if (img === '' || img === null) {
+          const nameSplit = name.split(' ');
           const nameInitials = [
             nameSplit[0][0],
             nameSplit[nameSplit.length - 1][0],
-          ].join("");
+          ].join('');
 
           return (
             <div
               className={`main-project__right__team__group--placeholder image-background-color--${colorPicker(
                 backgroundColor
-              )}`}
-            >
+              )}`}>
               {nameInitials}
             </div>
           );
@@ -127,78 +129,92 @@ const MainProject = () => {
       );
     });
   };
+  const {
+    loading: userInfoLoading,
+    error: userInfoError,
+    data: userInfoData,
+  } = useQuery(getCurrUser, {
+    variables: { id: userData ? userData.me.id : null },
+  });
+  if (loading) return <div>Loading</div>;
+  else if (error) return <div>{JSON.stringify(error)}</div>;
+  else {
+    console.log(userInfoData);
+    console.log(userInfoError);
 
-  return (
-    <div className="dashboard-projects">
-      <Navigation />
-      <div className="main-project">
-        <h1>Mattamy Development</h1>
-        <div className="main-project__left">
-          <div className="main-project__left__image">
-            <img
-              src={building1}
-              alt="building-1"
-              className="main-project__left__image--1"
-            />
-            <img
-              src={interior1}
-              alt="interior-1"
-              className="main-project__left__image--2"
-            />
-            <img
-              src={interior2}
-              alt="interior-2"
-              className="main-project__left__image--3"
-            />
-            <img
-              src={interior3}
-              alt="interior-3"
-              className="main-project__left__image--4"
-            />
+    console.log(data.projects[0]);
+    var currProject = data.projects[0];
+    return (
+      <div className="dashboard-projects">
+        <Navigation />
+        <div className="main-project">
+          <h1>{currProject.name}</h1>
+          <Link to="/test">bid now</Link>
+          <div className="main-project__left">
+            <div className="main-project__left__image">
+              <img
+                src={building1}
+                alt="building-1"
+                className="main-project__left__image--1"
+              />
+              <img
+                src={interior1}
+                alt="interior-1"
+                className="main-project__left__image--2"
+              />
+              <img
+                src={interior2}
+                alt="interior-2"
+                className="main-project__left__image--3"
+              />
+              <img
+                src={interior3}
+                alt="interior-3"
+                className="main-project__left__image--4"
+              />
+            </div>
+
+            <div className="main-project__left__details">
+              <ul className="main-project__left__details--1">
+                <li>Location</li>
+                <li>Project ID</li>
+                <li>Est. Duration</li>
+                <li>Budget</li>
+              </ul>
+              <ul className="main-project__left__details--2">
+                <li>
+                  {currProject.address}{' '}
+                  <span>
+                    {currProject.city}, {currProject.state} {currProject.zip}
+                  </span>
+                </li>
+                <li>{props.match.params.id}</li>
+                <li>{currProject.estTime}</li>
+                <li>${currProject.minBid}</li>
+              </ul>
+            </div>
+
+            <h3>Project Details</h3>
+            <p>{currProject.description}</p>
           </div>
 
-          <div className="main-project__left__details">
-            <ul className="main-project__left__details--1">
-              <li>Location</li>
-              <li>Project ID</li>
-              <li>Est. Completition Date</li>
-              <li>Budget</li>
-            </ul>
-            <ul className="main-project__left__details--2">
-              <li>
-                1015 Market Street <span>San Francisco, CA 94103</span>
-              </li>
-              <li>1924561</li>
-              <li>September 2021</li>
-              <li>$7.2 million</li>
-            </ul>
-          </div>
+          <div className="main-project__right">
+            <div className="main-project__right__cards main-project__right__bidding ">
+              <h3>Bidding Activity</h3>
 
-          <h3>Project Details</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis
-            aute irure dolor in reprehenderit in voluptate velit esse cillum
-            dolore eu fugiat nulla pariatur.
-          </p>
-        </div>
+              {renderBidding(bids, bidColors)}
+            </div>
 
-        <div className="main-project__right">
-          <div className="main-project__right__cards main-project__right__bidding ">
-            <h3>Bidding Activity</h3>
+            <div className="main-project__right__cards main-project__right__team ">
+              <h3>Project Team</h3>
 
-            {renderBidding(bids, bidColors)}
-          </div>
-
-          <div className="main-project__right__cards main-project__right__team ">
-            <h3>Project Team</h3>
-
-            {renderTeam(team, backgroundColor)}
+              {renderTeam(team, backgroundColor)}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default MainProject;
