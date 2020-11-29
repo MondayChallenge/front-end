68d13f8b195efbba9288fffd9c40c4a7bf59c546
varunsub <varun.subramanyan@gmail.com>
2020-11-29 12:59:31 -0800

    Changed representative to array

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,91 +1,90 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import Navigation from "../Navigation/Navigation";
-
+import Navigation from '../Navigation/Navigation';
 
 // import {AddProject} from '../../apollo/project';
 // import { Formik } from 'formik';
-import {AddProject} from '../../apollo/project'
+import { AddProject } from '../../apollo/project';
 import { useMutation } from '@apollo/client';
 
-  
 const Projects = () => {
-  
   const [organization, setOrgID] = useState(1);
-  const [name, setName] = useState("");
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
-  const [timezone, setTimezone] = useState("");
-  const [projectType, setProjectType] = useState("");
-  const [minBid, setMinBid] = useState("");
-  const [maxBudgetRange, setMaxBudgetRange] = useState("");
-  const [description, setDescription] = useState("");
-  const [country, setCountry] = useState("");
-  const [state, setState] = useState("");
-  const [city, setCity] = useState("");
-  const [zip, setZip] = useState("");
-  const [address, setAddress] = useState("");
+  const [name, setName] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [timezone, setTimezone] = useState('');
+  const [projectType, setProjectType] = useState('');
+  const [minBid, setMinBid] = useState('');
+  const [maxBudgetRange, setMaxBudgetRange] = useState('');
+  const [description, setDescription] = useState('');
+  const [country, setCountry] = useState('');
+  const [state, setState] = useState('');
+  const [city, setCity] = useState('');
+  const [zip, setZip] = useState('');
+  const [address, setAddress] = useState('');
   const [owner, setOwner] = useState(1);
   const [architect, setArchitect] = useState(1);
   const [manager, setManager] = useState(1);
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [representatives] = useState(sessionStorage.getItem('userId'));
-  const [createProject, {data}] = useMutation(AddProject,{
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [representatives] = useState([sessionStorage.getItem('userId')]);
+  const [createProject, { data }] = useMutation(AddProject, {
     onCompleted: (data) => {
-      console.log('succeed adding project', data.createProject.project.id)
-      return data
+      console.log('succeed adding project', data.createProject.project.id);
+      return data;
     },
-    onError: (error) => console.error("Error getting RegisterUser", error)
-  })
-  
-
+    onError: (error) => console.error('Error getting RegisterUser', error),
+  });
 
   const testData = {
-    "zip": zip,
-    "published_at": "2019-12-03T10:15:30Z",
-    "status": new Date()- Date.parse(endDate) > 0 ? "Closed to Bid" : new Date()- Date.parse(startDate) < 0 ? "Upcoming" :"Open to Bid",
-    "endDate": endDate,
-    "minBid": parseFloat(minBid),
-    "city": city,
-    "name": name,
-    "numStories": "2000",
-    "manager": manager,
-    "category": "Office",
-    "team": [1,2,3],
-    "estTime": "3 Month",
-    "representatives": representatives,
-    "description": description,
-    "email": email,
-    "invitations": [1,2,3],
-    "type": "Whatever",
-    "organization": organization,
-    "numBuildings": "1",
-    "country": country,
-    "architect": architect,
-    "address": address,
-    "squareFootage": "100",
-    "maxBudgetRange": parseFloat(maxBudgetRange),
-    "owner": owner,
-    "state": state,
-    "startDate": startDate,
-    "phone": phone,
-    "projectType": projectType
-  }
-  const handleSubmit = (event)=>{
+    zip: zip,
+    published_at: '2019-12-03T10:15:30Z',
+    status:
+      new Date() - Date.parse(endDate) > 0
+        ? 'Closed to Bid'
+        : new Date() - Date.parse(startDate) < 0
+        ? 'Upcoming'
+        : 'Open to Bid',
+    endDate: endDate,
+    minBid: parseFloat(minBid),
+    city: city,
+    name: name,
+    numStories: '2000',
+    manager: manager,
+    category: 'Office',
+    team: [1, 2, 3],
+    estTime: '3 Month',
+    representatives: representatives,
+    description: description,
+    email: email,
+    invitations: [1, 2, 3],
+    type: 'Whatever',
+    organization: organization,
+    numBuildings: '1',
+    country: country,
+    architect: architect,
+    address: address,
+    squareFootage: '100',
+    maxBudgetRange: parseFloat(maxBudgetRange),
+    owner: owner,
+    state: state,
+    startDate: startDate,
+    phone: phone,
+    projectType: projectType,
+  };
+  const handleSubmit = (event) => {
     console.log('click button');
     event.preventDefault();
     const token = sessionStorage.getItem('jwtToken');
-    sessionStorage.setItem('jwt',token);
+    sessionStorage.setItem('jwt', token);
     // sessionStorage.setItem('jwt',"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjA2Mjc4MDk3LCJleHAiOjE2MDg4NzAwOTd9.efVO2-okLs2ZanNEBWnDKPp3gC4fnh-AY7Rx6ZXEUyI");
-    createProject({variables: testData});
-  }
-
+    createProject({ variables: testData });
+  };
 
   return (
     <div className="dashboard-projects">
       <Navigation />
-      <form action="" className="project-form" onSubmit={handleSubmit} >
+      <form action="" className="project-form" onSubmit={handleSubmit}>
         <h1 className="form-section-header">Project Information</h1>
 
         <p className="form-section-subheader ">
